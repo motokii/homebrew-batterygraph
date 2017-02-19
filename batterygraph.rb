93a26730f0d28e2fe017a832d2583a7e8b8dd18c
motoki nagaoka <motoki.23ar@gmail.com>
2017-02-19 20:07:18 +0900

    brew install batterygraph

---
+++ batterygraph.rb
@@ -0,0 +1,41 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Batterygraph < Formula
+  desc "Display graph of MacBook battery"
+  homepage "https://github.com/motokii/batterygraph"
+  url "https://github.com/motokii/batterygraph/archive/v0.3.3.tar.gz"
+  sha256 "16598ce30be3393faa590b6dca9938f75aa7552aaa1accbbcb235bf4b7744798"
+  head "https://github.com/motokii/batterygraph.git"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+#    system "./configure", "--disable-debug",
+#                          "--disable-dependency-tracking",
+#                          "--disable-silent-rules",
+#                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    #system "make", "install" # if this fails, try separate make/make install steps
+  bin.install Dir['src/*']
+#bin.install "src/batterygraph"
+
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test batterygraph`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
